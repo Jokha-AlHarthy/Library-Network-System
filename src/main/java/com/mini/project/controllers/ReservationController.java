5c3ac0652ce666c513db037a08e057c55b587b84
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 15:12:42 +0400

    feat(controllers): Updating ReservationController.java file

--- src/main/java/com/mini/project/controllers/ReservationController.java
+++ src/main/java/com/mini/project/controllers/ReservationController.java
@@ -1,7 +1,9 @@
 package com.mini.project.controllers;
 
+import com.mini.project.dto.ReservationDTO;
 import com.mini.project.entities.Reservation;
 import com.mini.project.services.ReservationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,27 +21,29 @@ public class ReservationController {
     }
 
     @PostMapping("add")
-    public Long addReservation(@RequestParam Date reservationDate,
-                               @RequestParam String status) {
-        return reservationService.addReservation(reservationDate, status);
+    public Long addReservation(@Valid @RequestBody ReservationDTO dto) {
+        return reservationService.addReservation(
+                dto.getReservationDate(),
+                dto.getStatus());
     }
 
     @GetMapping("getAll")
-    public List<Reservation> getAllReservations() {
-        return reservationService.getAllReservations();
+    public List<ReservationDTO> getAllReservations() {
+        List<ReservationDTO> reservations = ReservationDTO.convertToDTO(reservationService.getAllReservations());
+        return reservations;
     }
 
     @GetMapping("getById")
-    public Reservation getById(@RequestParam Long id) {
-        return reservationService.getById(id);
+    public ReservationDTO getById(@RequestParam Long id) {
+        return ReservationDTO.convertToDTO(reservationService.getById(id));
     }
 
     @PutMapping("update")
-    public Reservation updateReservation(@RequestParam Long id,
-                                         @RequestParam Date updateReservationDate,
-                                         @RequestParam String updateStatus) throws Exception {
-        return reservationService.updateReservation(id,
-                updateReservationDate, updateStatus);
+    public ReservationDTO updateReservation(@Valid @RequestBody ReservationDTO dto) throws Exception {
+        return ReservationDTO.convertToDTO(reservationService.updateReservation(
+                dto.getReservationId(),
+                dto.getReservationDate(),
+                dto.getStatus()));
     }
 
     @DeleteMapping("deleteById")
