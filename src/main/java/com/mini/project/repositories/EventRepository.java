9c1e0b83da4587dfe4ce7eb496061a3644ddbccf
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 11:48:32 +0400

    feat(respoistories): Adding EventRepository.java file

--- src/main/java/com/mini/project/repositories/EventRepository.java
+++ src/main/java/com/mini/project/repositories/EventRepository.java
@@ -1,9 +1,17 @@
 package com.mini.project.repositories;
-
 import com.mini.project.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("SELECT e FROM Event e WHERE e.isActive=true")
+    List<Event> getAllEvents();
+
+    @Query("SELECT e FROM Event e WHERE e.isActive=true AND e.id=:event")
+    Event getById(@Param("event") Long id);
 }
