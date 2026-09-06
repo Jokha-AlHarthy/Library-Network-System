cc8a956547931fda6ddfed1efb91b4a04b92805a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:27:48 +0400

    feat(repositories): Adding BookRepository.java file

---
+++ src/main/java/com/mini/project/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
