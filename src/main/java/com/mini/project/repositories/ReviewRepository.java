a48fec97c76da4931be6efccf7dfb5ee9c501fec
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:30:27 +0400

    feat(repositories): Adding ReviewRepository.java file

---
+++ src/main/java/com/mini/project/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
