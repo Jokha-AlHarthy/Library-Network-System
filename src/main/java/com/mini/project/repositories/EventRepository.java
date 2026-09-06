74bc0f6cb9b3784c85ab3efa8635025c39062a2d
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:28:45 +0400

    feat(repositories): Adding EventRepository.java file

---
+++ src/main/java/com/mini/project/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
