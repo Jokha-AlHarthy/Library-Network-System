b0f3d2df67ee537e87b922552336cfd5af95c11f
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 14:05:55 +0400

    feat(respoistories): Adding ReservationRepository.java file

--- src/main/java/com/mini/project/repositories/ReservationRepository.java
+++ src/main/java/com/mini/project/repositories/ReservationRepository.java
@@ -1,9 +1,19 @@
 package com.mini.project.repositories;
 
+import com.mini.project.entities.Author;
 import com.mini.project.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.isActive=true")
+    List<Reservation> getAllReservations();
+
+    @Query("SELECT r FROM Reservation r WHERE r.isActive=true AND r.id=:reservation")
+    Reservation getById(@Param("reservation") Long id);
 }
