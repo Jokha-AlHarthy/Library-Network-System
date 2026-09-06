a40931945193b2985fcf25221c41021b756cc8b0
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:28:03 +0400

    feat(repositories): Adding BranchRepository.java file

---
+++ src/main/java/com/mini/project/repositories/BranchRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+}
