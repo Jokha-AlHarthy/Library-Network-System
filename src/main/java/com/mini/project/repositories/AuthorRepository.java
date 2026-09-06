3a73bd74d5d8b031ad1aee873d43c8c7f99e139a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 11:05:08 +0400

    feat(repositories): Updating AuthorRepository.java file

--- src/main/java/com/mini/project/repositories/AuthorRepository.java
+++ src/main/java/com/mini/project/repositories/AuthorRepository.java
@@ -2,8 +2,17 @@ package com.mini.project.repositories;
 
 import com.mini.project.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("SELECT a FROM Author a WHERE a.isActive=true")
+    List<Author> getAllAuthors();
+
+    @Query("SELECT a FROM Author a WHERE a.isActive=true AND id=:author")
+    Author getById(@Param("author") Long id);
 }
