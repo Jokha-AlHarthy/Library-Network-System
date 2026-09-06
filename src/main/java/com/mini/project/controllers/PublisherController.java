962a683106dae01353fb4519b7bbc8f9957e3ea3
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 15:12:25 +0400

    feat(controllers): Updating PubloisherController.java file

--- src/main/java/com/mini/project/controllers/PublisherController.java
+++ src/main/java/com/mini/project/controllers/PublisherController.java
@@ -1,7 +1,9 @@
 package com.mini.project.controllers;
 
+import com.mini.project.dto.PublisherDTO;
 import com.mini.project.entities.Publisher;
 import com.mini.project.services.PublisherService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,29 +20,31 @@ public class PublisherController {
     }
 
     @PostMapping("add")
-    public Long addPublisher(@RequestParam String name,
-                             @RequestParam String address,
-                             @RequestParam String contactEmail) {
-        return publisherService.addPublisher(name, address, contactEmail);
+    public Long addPublisher(@Valid @RequestBody PublisherDTO dto) {
+        return publisherService.addPublisher(
+                dto.getPublisherName(),
+                dto.getPublisherAddress(),
+                dto.getPublisherContactEmail());
     }
 
     @GetMapping("getAll")
-    public List<Publisher> getAllPublishers() {
-        return publisherService.getAllPublishers();
+    public List<PublisherDTO> getAllPublishers() {
+        List<PublisherDTO> publishers = PublisherDTO.convertToDTO(publisherService.getAllPublishers());
+        return publishers;
     }
 
     @GetMapping("getById")
-    public Publisher getById(@RequestParam Long id) {
-        return publisherService.getById(id);
+    public PublisherDTO getById(@RequestParam Long id) {
+        return PublisherDTO.convertToDTO(publisherService.getById(id));
     }
 
     @PutMapping("update")
-    public Publisher updatePublisher(@RequestParam Long id,
-                                     @RequestParam String updateName,
-                                     @RequestParam String updateAddress,
-                                     @RequestParam String updateContactEmail) throws Exception {
-        return publisherService.updatePublisher(id, updateName,
-                updateAddress, updateContactEmail);
+    public PublisherDTO updatePublisher(@Valid @RequestBody PublisherDTO dto) throws Exception {
+        return PublisherDTO.convertToDTO(publisherService.updatePublisher(
+                dto.getPublisherId(),
+                dto.getPublisherName(),
+                dto.getPublisherAddress(),
+                dto.getPublisherContactEmail()));
     }
 
     @DeleteMapping("deleteById")
