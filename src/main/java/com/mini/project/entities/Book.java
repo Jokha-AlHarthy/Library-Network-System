ac30cb15a37413fd3ec8a649ea805634c3837e2c
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 09:23:52 +0400

    feat(entities): Adding Book.java file

--- src/main/java/com/mini/project/entities/Book.java
+++ src/main/java/com/mini/project/entities/Book.java
@@ -1,4 +1,40 @@
 package com.mini.project.entities;
 
-public class Book {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Book extends BaseClass{
+    private String title;
+    private String isbn;
+    private Integer totalCopies;
+    private Integer availableCopies;
+    @ManyToOne
+    private Branch branch;
+
+    @ManyToOne
+    private Author author;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Publisher publisher;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Loan> loans;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Reservation> reservations;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Review> reviews;
 }
