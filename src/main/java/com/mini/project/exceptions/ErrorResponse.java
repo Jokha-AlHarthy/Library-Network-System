84981f75bc6de7ec820c554e1357f95f457e0b6d
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 16:15:21 +0400

    feat(exceptions): Updating ErrorResponse.java file

--- src/main/java/com/mini/project/exceptions/ErrorResponse.java
+++ src/main/java/com/mini/project/exceptions/ErrorResponse.java
@@ -1,11 +1,12 @@
 package com.mini.project.exceptions;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public record ErrorResponse(
         int status,
         String error,
         String message,
-        Date timestamp
+        LocalDateTime timestamp
 ) {
 }
