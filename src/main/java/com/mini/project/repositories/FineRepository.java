226b842d3032c2880c948c198d4509054855d459
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 12:14:44 +0400

    feat(respoistories): Adding FineRepository.java file

--- src/main/java/com/mini/project/repositories/FineRepository.java
+++ src/main/java/com/mini/project/repositories/FineRepository.java
@@ -2,8 +2,17 @@ package com.mini.project.repositories;
 
 import com.mini.project.entities.Fine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FineRepository extends JpaRepository<Fine, Long> {
+    @Query("SELECT f FROM Fine f WHERE f.isActive=true")
+    List<Fine> getAllFines();
+
+    @Query("SELECT f FROM Fine f WHERE f.isActive=true AND f.id=:fine")
+    Fine getById(@Param("fine") Long id);
 }
