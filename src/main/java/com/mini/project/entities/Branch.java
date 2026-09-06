8bf8ec2a1caa80fdf4be20791667762c88a1c442
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 09:10:18 +0400

    feat(entities): Adding Brach.java file

--- src/main/java/com/mini/project/entities/Branch.java
+++ src/main/java/com/mini/project/entities/Branch.java
@@ -1,4 +1,26 @@
 package com.mini.project.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class Branch {
+    private String name;
+    private String location;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Books> books;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Members> members;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Staff> staffs;
 }
