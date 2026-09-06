96eb743d5814d991ce89bf4985482908a0501dea
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:30:40 +0400

    feat(repositories): Adding StaffRepository.java file

---
+++ src/main/java/com/mini/project/repositories/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+}
