1321791604899e4ad743b0718f9673d1fe9dd7ba
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 12:22:51 +0400

    feat(respoistories): Adding LoanRepository.java file

--- src/main/java/com/mini/project/repositories/LoanRepository.java
+++ src/main/java/com/mini/project/repositories/LoanRepository.java
@@ -1,9 +1,19 @@
 package com.mini.project.repositories;
 
+import com.mini.project.entities.Author;
 import com.mini.project.entities.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    @Query("SELECT l FROM Loan l WHERE l.isActive=true")
+    List<Loan> getAllLoans();
+
+    @Query("SELECT l FROM Loan l WHERE l.isActive=true AND l.id=:loan")
+    Loan getById(@Param("loan") Long id);
 }
