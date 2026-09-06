f5ee4bc1552f3d81eb4e5e573ea99c4a8ae9f6d6
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 12:39:05 +0400

    feat(respoistories): Adding PublisherRepository.java file

--- src/main/java/com/mini/project/repositories/PublisherRepository.java
+++ src/main/java/com/mini/project/repositories/PublisherRepository.java
@@ -1,9 +1,19 @@
 package com.mini.project.repositories;
 
+import com.mini.project.entities.Author;
 import com.mini.project.entities.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+    @Query("SELECT p FROM Publisher p WHERE p.isActive=true")
+    List<Publisher> getAllPublishers();
+
+    @Query("SELECT p FROM Publisher p WHERE p.isActive=true AND p.id=:publisher")
+    Publisher getById(@Param("publisher") Long id);
 }
