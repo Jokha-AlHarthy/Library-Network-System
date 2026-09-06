f72762c3d4a7e947a9d177f1730dac8b5e418d36
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 11:17:42 +0400

    feat(repositories): Adding BookRepository.java file

--- src/main/java/com/mini/project/repositories/BookRepository.java
+++ src/main/java/com/mini/project/repositories/BookRepository.java
@@ -1,9 +1,19 @@
 package com.mini.project.repositories;
 
+import com.mini.project.entities.Author;
 import com.mini.project.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT b FROM Book b WHERE b.isActive=true")
+    List<Book> getAllBooks();
+
+    @Query("SELECT b FROM Book b WHERE b.isActive=true AND b.id=:book")
+    Book getById(@Param("book") Long id);
 }
