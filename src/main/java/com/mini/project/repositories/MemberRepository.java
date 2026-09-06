084527980e9ff4bddc83e8173a5b899d867d7c97
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 12:31:01 +0400

    feat(respoistories): Adding MemberRepository.java file

--- src/main/java/com/mini/project/repositories/MemberRepository.java
+++ src/main/java/com/mini/project/repositories/MemberRepository.java
@@ -1,9 +1,19 @@
 package com.mini.project.repositories;
 
+import com.mini.project.entities.Author;
 import com.mini.project.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("SELECT m FROM Member m WHERE m.isActive=true")
+    List<Member> getAllMembers();
+
+    @Query("SELECT m FROM Member m WHERE m.isActive=true AND m.id=:member")
+    Member getById(@Param("member") Long id);
 }
