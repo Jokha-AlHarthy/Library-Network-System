8c7f60665598df0cc56cc9508f6bf2ba168814ba
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 15:46:48 +0400

    feat(repositories): Updating LoanRepository.java file

--- src/main/java/com/mini/project/repositories/LoanRepository.java
+++ src/main/java/com/mini/project/repositories/LoanRepository.java
@@ -16,4 +16,35 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     @Query("SELECT l FROM Loan l WHERE l.isActive=true AND l.id=:loan")
     Loan getById(@Param("loan") Long id);
+
+    @Query("""
+            SELECT COUNT(l)
+            FROM Loan l
+            WHERE l.member.id = :memberId
+            AND l.isReturned = false
+            AND l.isActive = true
+            """)
+    long countActiveLoansByMember(@Param("memberId") Long memberId);
+
+    @Query("""
+        SELECT l FROM Loan l
+        WHERE l.id = :loanId
+        AND l.isActive = true
+        """)
+    Loan findActiveLoan(@Param("loanId") Long loanId);
+
+    @Query("SELECT l FROM Loan l WHERE l.isActive=true AND l.isReturned=false")
+    List<Loan> getActiveLoans();
+
+    @Query("SELECT l FROM Loan l WHERE l.isActive=true AND l.isReturned=false AND l.dueDate < CURRENT_DATE")
+    List<Loan> getOverdueLoans();
+
+    @Query("""
+       SELECT l.book.id, COUNT(l.id)
+       FROM Loan l
+       WHERE l.isActive=true
+       GROUP BY l.book.id
+       ORDER BY COUNT(l.id) DESC
+       """)
+    List<Object[]> getMostBorrowed();
 }
