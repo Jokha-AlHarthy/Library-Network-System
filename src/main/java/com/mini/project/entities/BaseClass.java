b6a8410c6feb888f8bdb37fa6ed9d2525bbc354a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 09:02:19 +0400

    feat(entities): Adding BaseClass.java file

---
+++ src/main/java/com/mini/project/entities/BaseClass.java
@@ -0,0 +1,16 @@
+package com.mini.project.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class BaseClass {
+    private UUID id;
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
+}
