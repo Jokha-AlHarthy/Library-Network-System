6f234fa8bd38bf316dd785ce3dc1ce4e70aee6cf
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 09:15:29 +0400

    feat(entities): Adding Category.java file

---
+++ src/main/java/com/mini/project/entities/Category.java
@@ -0,0 +1,19 @@
+package com.mini.project.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Category extends BaseClass{
+    private String name;
+    private String description;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Books> books;
+}
