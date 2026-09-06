4a6f16265a87edd72bcecc9e39a880e667a4aaad
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:30:07 +0400

    feat(repositories): Adding ReservationRepository.java file

---
+++ src/main/java/com/mini/project/repositories/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
