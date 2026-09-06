539bb620ddd75dcba5594b389bfd6ea1708cef85
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:29:24 +0400

    feat(repositories): Adding MemberRepository.java file

---
+++ src/main/java/com/mini/project/repositories/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
