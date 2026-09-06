b20fca834858366c8a7bc0c4a740bf44d15a26d0
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:28:28 +0400

    feat(repositories): Adding CategoryRepository.java file

---
+++ src/main/java/com/mini/project/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
