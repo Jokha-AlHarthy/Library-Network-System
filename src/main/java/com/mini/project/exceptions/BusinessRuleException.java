6a9a4eba30a029264afb932bee6035c503f525a1
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 16:14:50 +0400

    feat(exceptions): Adding BusinessRuleException.java file

---
+++ src/main/java/com/mini/project/exceptions/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package com.mini.project.exceptions;
+
+public class BusinessRuleException extends RuntimeException{
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
