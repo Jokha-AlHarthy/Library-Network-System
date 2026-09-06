e15c3abb000b6fb49a4c2a9a5244a268396d05e5
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 10:29:11 +0400

    feat(repositories): Adding LoanRepository.java file

---
+++ src/main/java/com/mini/project/repositories/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.mini.project.repositories;
+
+import com.mini.project.entities.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
