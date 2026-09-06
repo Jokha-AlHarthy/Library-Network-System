5cd776a2f52c45cb7c0666d7ca96ff49307f56fc
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-09-06 15:43:37 +0400

    feat(controllers): Updating LoanController.java file

--- src/main/java/com/mini/project/controllers/LoanController.java
+++ src/main/java/com/mini/project/controllers/LoanController.java
@@ -1,5 +1,6 @@
 package com.mini.project.controllers;
 
+import com.mini.project.dto.BorrowDTO;
 import com.mini.project.dto.LoanDTO;
 import com.mini.project.entities.Loan;
 import com.mini.project.services.LoanService;
@@ -29,6 +30,18 @@ public class LoanController {
                 dto.getIsReturned());
     }
 
+    @PostMapping("borrow")
+    public LoanDTO borrowBook(
+            @Valid @RequestBody BorrowDTO dto) {
+
+        Loan loan = loanService.borrowBook(
+                dto.getMemberId(),
+                dto.getBookId()
+        );
+
+        return LoanDTO.convertToDTO(loan);
+    }
+
     @GetMapping("getAll")
     public List<LoanDTO> getAllLoans() {
         List<LoanDTO> loans = LoanDTO.convertToDTO(loanService.getAllLoans());
@@ -40,6 +53,25 @@ public class LoanController {
         return LoanDTO.convertToDTO(loanService.getById(id));
     }
 
+    @GetMapping("active")
+    public List<LoanDTO> getActiveLoans() {
+        return LoanDTO.convertToDTO(
+                loanService.getActiveLoans()
+        );
+    }
+
+    @GetMapping("overdue")
+    public List<LoanDTO> getOverdueLoans() {
+        return LoanDTO.convertToDTO(
+                loanService.getOverdueLoans()
+        );
+    }
+
+    @GetMapping("mostBorrowed")
+    public List<Object[]> getMostBorrowed() {
+        return loanService.getMostBorrowed();
+    }
+
     @PutMapping("update")
     public LoanDTO updateLoan(@Valid @RequestBody LoanDTO dto) throws Exception {
         return LoanDTO.convertToDTO(loanService.updateLoan(
@@ -48,6 +80,14 @@ public class LoanController {
                 dto.getDueDate(),
                 dto.getReturnDate(),
                 dto.getIsReturned()));
+    }
+
+    @PutMapping("return")
+    public LoanDTO returnBook(@RequestParam Long loanId) throws Exception {
+
+        Loan loan = loanService.returnBook(loanId);
+
+        return LoanDTO.convertToDTO(loan);
     }
 
     @DeleteMapping("deleteById")
